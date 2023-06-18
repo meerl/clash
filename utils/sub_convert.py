584b8a2e207b44da1bd8a21b3b561d0060d0d867
tony-sung <97819349+tony-sung@users.noreply.github.com>
2023-06-18 04:06:33 +0000

    update convert

--- utils/sub_convert.py
+++ utils/sub_convert.py
@@ -33,21 +33,27 @@ class sub_convert():
                     resp = s.get(converted_url)
                     # 如果解析出错，将原始链接内容拷贝下来
                     if 'No nodes were found!' in resp.text or url in resp.text or 'Error code' in resp.text or 'An error' in resp.text:
-                        print(f"\n未发现有效配置, 订阅链接: {url} 转换链接:{converted_url} 回复消息: {resp.text}\n")
+                        print(f"\n未发现有效配置, 订阅链接: {url} 转换链接:{converted_url}\n")
                         if server_host is server_host_list[-1]:
                             print(f"\n无法转换订阅链接: {url}, 使用内置方法转换\n")
                             resp = s.get(url, verify=None, timeout=10)
                         else:
                             continue
-                    print(f'\n格式化{url} 返回数据开始...')
-                    node_list_formated = sub_convert.format(resp.text)
-                    print(f'\n格式化{url} 返回数据结束...')
-                    sub_content.append(node_list_formated)
-                    break
                 except Exception:
-                    # 链接有问题，直接返回原始错误
-                    print(f'\n{url} 网络错误 检查订阅转换服务器是否失效: {converted_url}\n')
-                    continue
+                    if server_host is server_host_list[-1]:
+                        print(f"\n无法转换订阅链接: {url}, 使用内置方法转换\n")
+                        try:
+                            resp = s.get(url, verify=None, timeout=10)
+                        except Exception:
+                            print(f'\n{url} 网络错误 检查订阅转换服务器是否失效: {converted_url}\n')
+                    else:
+                        print(f'\n{url} 网络错误 检查订阅转换服务器是否失效: {converted_url}\n')
+                        continue
+                print(f'\n格式化{url} 返回数据开始...')
+                node_list_formated = sub_convert.format(resp.text)
+                print(f'\n格式化{url} 返回数据结束...')
+                sub_content.append(node_list_formated)
+                break
         sub_content_all = ''.join(sub_content)
         return sub_content_all
 
